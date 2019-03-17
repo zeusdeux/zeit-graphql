9a769f19dc53a0565b9fcdc9ef41eeb35c9cf6e4
Mudit Ameta <zeusdeux@gmail.com>
2019-03-17 16:04:39 +0100

    Conditionally enable introspection in prod

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,16 @@ import { ApolloServer } from 'apollo-server'
 import { schema } from './schema'
 import { ZeitAPI } from './zeitHttpDataSource'
 
+function enableIntrospectionInProd() {
+  if (process.env.NODE_ENV !== 'production') {
+    return true
+  }
+
+  return process.env.ENABLE_INTROSPECTION === 'true' ? true : false
+}
+
 const server = new ApolloServer({
+  introspection: enableIntrospectionInProd(),
   schema,
   context: ({ req }) => ({
     authToken: req.headers.authorization ? req.headers.authorization.trim() : ''
